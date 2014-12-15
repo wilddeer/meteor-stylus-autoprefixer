6d923ef026655cce98e8dddae34703298fc25b06
Oleg Korsunsky <jatruadmin@gmail.com>
2014-12-15 14:20:45 +0000

    custom stylus + autoprefixer remake

--- package.js
+++ package.js
@@ -1,22 +1,19 @@
 Package.describe({
-  summary: " Stylus along with Jeet, Rupture, Axis and Autoprefixer! ",
-  version: "0.1.0",
-  git: " https://github.com/jsnoble1/meteor-multi-styl "
+    name: 'wilddeer:stylus-autoprefixer',
+    summary: 'Stylus + Autoprefixer',
+    version: '1.0.0',
+    git: 'https://github.com/wilddeer/meteor-stylus-autoprefixer.git'
 });
 
 
-Package._transitional_registerBuildPlugin({
-  name: "compileStylus",
+Package.registerBuildPlugin({
+  name: 'compileStylus',
   use: [],
   sources: [
     'plugin/compile-stylus.js'
   ],
   npmDependencies: {
-    "stylus": "0.49.1",
-    "nib": "1.0.4",
-    "autoprefixer-stylus": "0.3.0",
-    "jeet": "6.1.2",
-    "rupture": "0.4.0",
-    "axis": "0.3.0"
+    'stylus': '0.49.3',
+    'autoprefixer-stylus': '0.4.0'
   }
 });
